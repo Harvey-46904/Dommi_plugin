c1e12259a88a55e563d2a3016d00dd00f167d135
Harvey-46904 <harveympv@hotmail.com>
2021-12-24 08:44:18 -0500

    creacion de formulario

--- dommi_plugin.php
+++ dommi_plugin.php
@@ -9,21 +9,36 @@ Author URI: http://hache-r.com.co/
 License: GPLv2 
 */
 require __DIR__ . '/vendor/autoload.php';
+require_once(ABSPATH.'wp-includes/pluggable.php');
+
 use Automattic\WooCommerce\Client;
 
 
 add_shortcode('dommi-domicilios', 'formulario_domicilio');
-add_action( 'wp_head', 'cabezera' );
+add_action( 'wp_head', 'url_actual' );
 add_action( 'wp_footer', 'your_function' );
-
+add_shortcode('domicilios_dommi', 'domicilio_Dommi');
 function your_function() {
     ?>
 
     <?php
 }
-function cabezera(){
-  echo "sebas";
-  
+
+function url_actual(){
+  global $wp;
+  $url_actual = $slug = basename(get_permalink());
+  echo $url_actual;
+}
+function comprobar_sesion(){
+  if(is_user_logged_in()){
+    //$hf_user= wp_get_current_user();
+    //$hf_username = $hf_user->user_login;
+    //echo "Usuario ".$hf_user->user_login." correo ".$hf_user->user_email;
+    //print_r($hf_user);
+    return true;
+  }else{
+    echo do_shortcode( ' [woocommerce_my_account] ' );
+  }
 }
 
 
@@ -88,37 +103,100 @@ function formulario_domicilio()
             ]
         ];
 
-$woocommerce = new Client(
-    'https://dommi.net/', 
-    'ck_ae1b5bce8346b9fe963511a6cfca17512a98f364', 
-    'cs_5f9d813b3fa984ed646fcca5451587863cb958c4',
-    [
-        'version' => 'wc/v3',
-    ]
-);
-     
-    
+  $woocommerce = new Client(
+      'https://dommi.net/', 
+      'ck_ae1b5bce8346b9fe963511a6cfca17512a98f364', 
+      'cs_5f9d813b3fa984ed646fcca5451587863cb958c4',
+      [
+          'version' => 'wc/v3',
+      ]
+  );
       
-    $woocommerce->post('orders', $data);
-        echo "<p class='exito'><b>Tus datos han sido registrados</b>. Gracias 
-            por tu interés. En breve contactaré contigo.<p>";
-    }
+      
+        
+      $woocommerce->post('orders', $data);
+          echo "<p class='exito'><b>Tus datos han sido registrados</b>. Gracias 
+              por tu interés. En breve contactaré contigo.<p>";
+      }
 
-////formulario
-    ob_start();
-   
-    ?>
-    <div id="form-domicilios" >
+  ////formulario
+      ob_start();
+    
+      ?>
+      <div id="form-domicilios" >
+      
+      <form action="<?php get_the_permalink(); ?>" method="POST" >
+        <div class="form-group">
+          <label style="color:#390066">Nombres</label>
+          <input type="text" class="form-control" name="nombres">
+        </div>
+        <div class="form-group">
+          <label style="color:#390066">Apellidos</label>
+          <input type="text" class="form-control" name="apellidos">
+        </div>
+        <div class="form-group">
+          <label style="color:#390066">Teléfono / Celular</label>
+          <input type="text" class="form-control" name="contacto">
+        </div>
+        <br>
+        <div class="form-group">
+          <br>
+          <label style="color:#390066">Correo electrónico</label>
+          <input type="email" class="form-control" name="email">
+        </div>
+        <div class="form-group">
+          <label style="color:#390066">Dirección donde recogemos</label>
+          <input type="text" class="form-control" name="recogida">
+        </div>
+        
+        <div class="form-group">
+          <label style="color:#390066">¿Qué deseas?</label>
+          <textarea class="form-control" id="deseo" rows="3" name="deseo"></textarea>
+        </div>
+        
+      
+        <div class="form-group">
+          <label style="color:#390066">¿Nombre de quien recibe?</label>
+          <input type="text" class="form-control" name="nombre_recibe">
+        </div>
+        <div class="form-group">
+          <label style="color:#390066">Apellido de quien recibe?</label>
+          <input type="text" class="form-control" name="apellido_recibe">
+        </div>
+        <br>
+        <div class="form-group">
+          <label style="color:#390066">Dirección donde entregamos</label>
+          <input type="text" class="form-control" name="entrega">
+        </div>
+        <br>
+      
+      
+        <br>
+        <button type="submit" class="btn btn-primary">SOLICITAR DOMICILIO</button>
+      </form>
+      
+      </script>
+      </div>
+
+      <?php
+      
+      // Devuelve el contenido del buffer de salida
+      return ob_get_clean();
+}
+
+
+function domicilio_Dommi(){
+  ob_start();
+  if(is_user_logged_in()){
+  ?>
+  <div id="form-domicilios" >
     
     <form action="<?php get_the_permalink(); ?>" method="POST" >
       <div class="form-group">
         <label style="color:#390066">Nombres</label>
         <input type="text" class="form-control" name="nombres">
       </div>
-      <div class="form-group">
-        <label style="color:#390066">Apellidos</label>
-        <input type="text" class="form-control" name="apellidos">
-      </div>
+     
       <div class="form-group">
         <label style="color:#390066">Teléfono / Celular</label>
         <input type="text" class="form-control" name="contacto">
@@ -136,17 +214,14 @@ $woocommerce = new Client(
       
       <div class="form-group">
         <label style="color:#390066">¿Qué deseas?</label>
-        <textarea class="form-control" id="deseo" rows="3" name="deseo"></textarea>
+        <textarea class="form-control" id="deseo" rows="3" name="deseo" placeholder="Por favor escribe las indicaciones para el mensajero, se claro y preciso en la información
+"></textarea>
       </div>
       
      
       <div class="form-group">
-        <label style="color:#390066">¿Nombre de quien recibe?</label>
+        <label style="color:#390066">¿Nombres de quien recibe?</label>
         <input type="text" class="form-control" name="nombre_recibe">
-      </div>
-      <div class="form-group">
-        <label style="color:#390066">Apellido de quien recibe?</label>
-        <input type="text" class="form-control" name="apellido_recibe">
       </div>
       <br>
       <div class="form-group">
@@ -154,8 +229,16 @@ $woocommerce = new Client(
         <input type="text" class="form-control" name="entrega">
       </div>
       <br>
-    
-     
+      <div class="form-group">
+        <label style="color:#390066">Teléfonoo de quien recibe</label>
+        <input type="text" class="form-control" name="telefono_recibe">
+      </div>
+      <br>
+      <div class="form-group">
+        <label style="color:#390066">Notas/Observaciones</label>
+        <textarea class="form-control" id="deseo" rows="3" name="deseo" placeholder="Información adicional o necesaria para el domicilio.
+"></textarea>
+      </div>
       <br>
       <button type="submit" class="btn btn-primary">SOLICITAR DOMICILIO</button>
     </form>
@@ -164,11 +247,13 @@ $woocommerce = new Client(
     </div>
 
     <?php
-     
     // Devuelve el contenido del buffer de salida
     return ob_get_clean();
+  }else {
+    echo 'Es necesario estar registrado';
+    echo do_shortcode( ' [woocommerce_my_account] ' );
+  }
 }
-
 
 
 
