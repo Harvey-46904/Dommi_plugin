3b06a668972311bfcafa618c7af7c416ff0030dd
Harvey-46904 <harveympv@hotmail.com>
2022-01-13 10:04:34 -0500

    cambio de control de ello

--- registro_usuario.php
+++ registro_usuario.php
@@ -115,8 +115,8 @@ function login_woo(){
 
 function validador_sesiones_imagen(){
     if(is_user_logged_in()){
-        jQuery("#con_sesion").show();
+       echo 'jQuery("#con_sesion").show();';
     }else{
-        jQuery("#sin_sesion").show();
+        echo 'jQuery("#sin_sesion").show();';
     }
 }
